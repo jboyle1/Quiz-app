769de0eed8117e207746cae8c65d330872b3988c
EC2 Default User <ec2-user@ip-172-31-41-165.us-east-2.compute.internal>
2019-10-27 11:31:55 +0000

    Loop function that continues the quiz as long as you keep playing the game.

--- quiz.py
+++ quiz.py
@@ -1,3 +1,5 @@
+#Simple menu function for quiz
+
 def show_menu():
     print('1. Ask questions')
     print('2. Add a question')
@@ -5,5 +7,20 @@ def show_menu():
     
     option = input('Enter option: ')
     return option
-    
-print(show_menu())
+
+#A loop that continues as long as you keep playing the game.
+
+def game_loop():
+    while True:
+        option = show_menu()
+        if option == '1':
+            print("You selected 'Ask questions'")
+        elif option == '2':
+            print("You selected 'Add a question'")
+        elif option == '3':
+            break
+        else :
+            print('Invalid option')
+        print('')
+        
+game_loop()
