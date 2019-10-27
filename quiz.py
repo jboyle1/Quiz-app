af35445f7e7afbcf56b8afb11ccd2824c259189b
EC2 Default User <ec2-user@ip-172-31-41-165.us-east-2.compute.internal>
2019-10-27 12:25:07 +0000

    Reformatted and finished quiz.

--- quiz.py
+++ quiz.py
@@ -1,66 +1,75 @@
-#Simple menu function for quiz
+
+# Please play quiz by inputting in terminal: python3 quiz.py 
+
+import difflib
 
 def show_menu():
-    print('1. Ask questions')
-    print('2. Add a question')
-    print('3. Exit game')
+    print("1. Ask questions")
+    print("2. Add a question")
+    print("3. Exit game")
     
-    option = input('Enter option: ')
+    option = input("Enter option: ")
     return option
-
-#Added the option to ask a question to our file. 
 
 def ask_questions():
     questions = []
-    answers=[]
+    answers = []
     
-    with open('questions.txt', 'r') as file:
+    with open("questions.txt", "r") as file:
         lines = file.read().splitlines()
-        
+    
     for i, text in enumerate(lines):
         if i%2 == 0:
             questions.append(text)
         else:
             answers.append(text)
     
-    for question, answer in zip(questions, answers):
-        guess = input(question + '> ')
+    number_of_questions = len(questions)
+    questions_and_answers = zip(questions, answers)
     
-#Added the option to add a question to our file. We want to be prompted for a question and answer, and then we want both of them to be appended to the questions.txt file.
-
+    score = 0
+            
+    for question, answer in questions_and_answers:
+        guess = input(question + "> ")
+        if guess == answer:
+            score += 1
+            print("right!")
+            print(score)
+        else:
+            if (sum([i[0] != ' '  for i in difflib.ndiff(guess, answer)]) / 2) <= 2:
+                print(sum([i[0] != ' '  for i in difflib.ndiff(guess, answer)]))
+                print("Spelled wrong, but right!")
+                score += 1
+                print(score)
+            else:
+                print("wrong!")
+    
+    print("You got {0} correct out of {1}".format(score, number_of_questions))
+    
 def add_question():
-    print('')
-    question = input('Enter a question\n> ')
+    print("")
+    question = input("Enter a question\n> ")
     
-    print('')
-    print('OK then, tell me the answer')
-    answer = input('{0}\n '.format(question))
+    print("")
+    print("OK then, tell me the answer")
+    answer = input("{0}\n> ".format(question))
     
-#Here I add this to the file. We'll open our questions.txt file for appending using the 'a' access mode flag.
-    
-    file = open('questions.txt', 'a')
-    file.write(question + '\n')
-    file.write(answer + '\n')
+    file = open("questions.txt","a")
+    file.write(question + "\n")
+    file.write(answer + "\n")
     file.close()
     
-#Now to actually get it working I need to call the function from inside our game loop if the user chooses option 2.
-
-#A loop that continues as long as you keep playing the game.
-
 def game_loop():
     while True:
         option = show_menu()
-        if option == '1':
+        if option == "1":
             ask_questions()
-        elif option == '2':
+        elif option == "2":
             add_question()
-        elif option == '3':
+        elif option == "3":
             break
-        else :
-            print('Invalid option')
-        print('')
+        else:
+            print("Invalid option")
+        print("")
         
 game_loop()
-
-
-
